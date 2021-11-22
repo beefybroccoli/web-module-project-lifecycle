3886311916521c7fa26071b5e08896b2b25535d4
beefybroccoli <tedkimemail@yahoo.com>
2021-11-22 14:18:11 -0800

    ...

--- .history/github-profile/src/component/constant_20210928150051.js
+++
@@ -1,7 +0,0 @@
-export const API_URL = "https://api.github.com/users/beefybroccoli";
-
-export const API_Call = (input_username)=>{
-    axios.get(API_URL+input_username)
-
-    }
-}
--- .history/github-profile/src/component/dashboard_20210928143836.js
+++
@@ -1,4 +0,0 @@
-import React from "react"
-import styled from 'styled-components'
-
-export default class Dashboard extends React.Component
